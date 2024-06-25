5ad5786638e82ddde39e214f9445c17581e26562
Ashonibare <Ashonibarevik@gmail.com>
2024-06-24 21:52:18 -0700

    some adjustments

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,34 +6,23 @@
  *
  * Return: returns zero ar the end
  *
- *      */
+ **/
 
 int main(void)
 
 {
+	char ch;
 
-		char ch;
+		for (ch = 'a'; ch <= 'z'; ch++)
+			{
+				putchar(ch);
 
-
-
-			for (ch = 'a'; ch <= 'z'; ch++)
-
-					{
-
-								putchar(ch);
-
-									}
-
+					}
 				for (ch = 'A'; ch <= 'Z'; ch++)
-
 						{
+							putchar(ch);
 
-									putchar(ch);
-
-										}
-
+								}
 					putchar('\n');
-
 						return (0);
-
 }
